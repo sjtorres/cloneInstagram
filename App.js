7f186a129e8c8208bfda6ebbbd1f024f64ccc939
Sandro Torres <sandrotorres.ti@gmail.com>
2020-04-14 10:32:04 -0300

    Add icons e Splash

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Mundo!</Text>
+      <Text>Bem Vindo Sandro Torres!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
