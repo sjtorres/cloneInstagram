5e964ec7090b2a08116ef6ca41f16a85d8065679
Sandro Torres <sandrotorres.ti@gmail.com>
2020-04-14 20:33:13 -0300

    Component Author add

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Image, Dimensions } from 'react-native';
 
+import Author from './Author'
+
 class Post extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Image source={this.props.image} style={styles.image} />      
+        <Author email='sandrotorres.ti@gmail.com' nickname='Sandro Torres' />
       </View>
     )
   }
